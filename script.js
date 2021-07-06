ccc5bf82ffaaddefa49b627821559af6b8c95517
Maiquera <maicolmenezes@gmail.com>
2021-07-06 19:18:01 -0300

    defining the end game

--- script.js
+++ script.js
@@ -48,6 +48,12 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction=="down") snake[0].y = 0;
     if(snake[0].y < 0 * box && direction=="up") snake[0].y = 16 * box;
 
+    for(i=1; i<snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobra();
@@ -67,8 +73,6 @@ function iniciarJogo(){
         comida.x = Math.floor(Math.random() * 15 + 1) * box;
         comida.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-
-    
 
     let newHead = {
         x: snakeX,
